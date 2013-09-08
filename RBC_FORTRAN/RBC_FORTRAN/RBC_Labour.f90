724fac3b93c636d1147300d4cc094bedbd93d794
matjohsch <m.schoen@wiso.uni-koeln.de>
2013-09-08 00:58:55 -0400

    in the night

--- RBC_FORTRAN/RBC_FORTRAN/RBC_Labour.f90
+++ RBC_FORTRAN/RBC_FORTRAN/RBC_Labour.f90
@@ -14,8 +14,8 @@ use  grid
 
 implicit none
 
-integer, parameter :: nGridCapital = 400
-integer, parameter :: nGridLabour =200
+integer, parameter :: nGridCapital = 100
+integer, parameter :: nGridLabour =50
 integer, parameter :: nGridProductivity = 5
 real (8), parameter :: curv=1.0
 real (8), parameter :: tolerance = 0.0000001
@@ -40,35 +40,36 @@ real(8), dimension(nGridLabour) :: vGridLabour
 real(8), dimension(nGridCapital,nGridProductivity) :: mValueFunction, mValueFunctionNew, mPolicyFunctionCapital, mPolicyFunctionLabour
 real(8), dimension(nGridCapital,nGridLabour,nGridProductivity) :: mOutput
 real(8), dimension(nGridCapital,nGridProductivity) :: expectedValueFunction
- 
+real(8), dimension(nGridLabour,nGridcapital) :: xxx
+integer,dimension(2)::res 
 !----------------------------------------------------------------
 ! 1. Calibration
 !----------------------------------------------------------------
         
 aalpha = 0.33333333333; ! Elasticity of output w.r.t
 bbeta = 0.95 ! Discount factor
-ddelta = 0.00
+ddelta = 0.09
 labourSteadyState=0.33333333333
 x=(aalpha*bbeta)/(1.0-bbeta+bbeta*ddelta)
-pphi = 6.0!((1.0-aalpha)/(labourSteadyState**2))/(1.0-ddelta*x) !6.72
+pphi = ((1.0-aalpha)/(labourSteadyState**2))/(1.0-ddelta*x) !6.72
 
 ! Productivity value
 vProductivity = (/0.9792, 0.9896, 1.0000, 1.0106, 1.0212/)
 
-!! Transition matrix
-!mTransition = reshape( (/0.9727, 0.0273, 0., 0., 0., &
-!0.0041, 0.9806, 0.0153, 0.0, 0.0, &
-!0.0, 0.0082, 0.9837, 0.0082, 0.0, &
-!0.0, 0.0, 0.0153, 0.9806, 0.0041, &
-!0.0, 0.0, 0.0, 0.0273, 0.9727 /), (/5,5/))
-   
 ! Transition matrix
-mTransition = reshape( (/0.0, 0.0, 1.0, 0.0, 0.0, &
-0.0, 0.0, 1.0, 0.0, 0.0, &
-0.0, 0.0, 1.0, 0.0, 0.0, &
-0.0, 0.0, 1.0, 0.0, 0.0, &
-0.0, 0.0, 1.0, 0.0, 0.0 /), (/5,5/))
-mTransition = transpose(mTransition)
+mTransition = reshape( (/0.9727, 0.0273, 0., 0., 0., &
+0.0041, 0.9806, 0.0153, 0.0, 0.0, &
+0.0, 0.0082, 0.9837, 0.0082, 0.0, &
+0.0, 0.0, 0.0153, 0.9806, 0.0041, &
+0.0, 0.0, 0.0, 0.0273, 0.9727 /), (/5,5/))
+   
+!! Transition matrix
+!mTransition = reshape( (/0.0, 0.0, 1.0, 0.0, 0.0, &
+!0.0, 0.0, 1.0, 0.0, 0.0, &
+!0.0, 0.0, 1.0, 0.0, 0.0, &
+!0.0, 0.0, 1.0, 0.0, 0.0, &
+!0.0, 0.0, 1.0, 0.0, 0.0 /), (/5,5/))
+!mTransition = transpose(mTransition)
 
 !----------------------------------------------------------------
 ! 2. Steady state
@@ -123,36 +124,46 @@ do while (maxDifference>tolerance)
 
             ! We start from previous choice (monotonicity of policy function)
             gridCapitalNextPeriod = 1  
-            
-            do nCapitalNextPeriod = gridCapitalNextPeriod,nGridCapital
+            xxx=0.0
+            do nCapitalNextPeriod = 1,nGridCapital !gridCapitalNextPeriod,nGridCapital
                 valueHighSoFar2 = -100000.0
-                do nLabour = 1,nGridCapital
+                do nLabour = 1,nGridLabour
                     
                     consumption = (1-ddelta)*vGridCapital(nCapital)+mOutput(nCapital,nLabour,nProductivity)-vGridCapital(nCapitalNextPeriod)
-                    valueProvisional2 = log(consumption)-pphi*0.5*vGridLabour(nLabour)**2+bbeta*expectedValueFunction(nCapitalNextPeriod,nProductivity) !(1.0-bbeta)*(
+                    
+                    xxx(nLabour,nCapitalNextperiod)  = log(consumption)-pphi*0.5*vGridLabour(nLabour)**2+bbeta*expectedValueFunction(nCapitalNextPeriod,nProductivity) !(1.0-bbeta)*(
+                    !valueProvisional2
                     if (consumption .lt. 0.0) then
+                        xxx(nLabour,nCapitalNextPeriod) = -100000.0
                         valueProvisional2 = -100000.0
                     end if
-                    if (valueProvisional2>=valueHighSoFar2) then ! we break when we have achieved the max
-                        valueHighSoFar2 = valueProvisional2
-                    else
-                        exit
-                    end if
+                    !if (valueProvisional2>=valueHighSoFar2) then ! we break when we have achieved the max
+                    !    valueHighSoFar2 = valueProvisional2
+                    !else
+                    !    x=2
+                    !    exit
+                    !end if
                 end do
-                if (valueProvisional2>=valueHighSoFar1) then ! we break when we have achieved the max
-                    valueHighSoFar1 = valueProvisional2
-                    labourChoice = vGridLabour(nLabour)
-                    capitalChoice = vGridCapital(nCapitalNextPeriod)
-                    gridCapitalNextPeriod = nCapitalNextPeriod
-                else
-                    exit
-                end if
+                !if (valueProvisional2>=valueHighSoFar1) then ! we break when we have achieved the max
+                !    valueHighSoFar1 = valueProvisional2
+                !    labourChoice = vGridLabour(nLabour)
+                !    capitalChoice = vGridCapital(nCapitalNextPeriod)
+                !    gridCapitalNextPeriod = nCapitalNextPeriod
+                !else
+                !    x=2
+                !    exit
+                !end if
             end do
-
-            mValueFunctionNew(nCapital,nProductivity) = valueHighSoFar1
-            mPolicyFunctionCapital(nCapital,nProductivity) = capitalChoice
-            mPolicyFunctionLabour(nCapital,nProductivity) = labourChoice
-            x=2  
+            res=maxloc(xxx)
+            
+            mValueFunctionNew(nCapital,nProductivity) = xxx(res(1),res(2))
+            mPolicyFunctionCapital(nCapital,nProductivity) = vGridCapital(res(2))
+            mPolicyFunctionLabour(nCapital,nProductivity) = vGridLabour(res(1))
+            
+            !mValueFunctionNew(nCapital,nProductivity) = valueHighSoFar1
+            !mPolicyFunctionCapital(nCapital,nProductivity) = capitalChoice
+            !mPolicyFunctionLabour(nCapital,nProductivity) = labourChoice
+            x=2
         end do
     end do
 
@@ -171,9 +182,9 @@ end do
   
 print *, 'Iteration:', iteration, 'Sup Diff:', MaxDifference
 print *, ' '
-print *, 'My check:', mPolicyFunctionCapital(200,3)
+print *, 'My check:', mPolicyFunctionCapital(50,3)
 print *, ' '
-print *, 'My check:', mPolicyFunctionLabour(200,3)
+print *, 'My check:', mPolicyFunctionLabour(50,3)
 print *, ' '
 !total = etime(elapsed)
 !
